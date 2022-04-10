1f5b08d004f5d01f86d72b3abe39a82d3937c67a
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-04-10 19:41:26 +0300

    Add problem solution

---
+++ AddandSubstract.js
@@ -0,0 +1,17 @@
+function addAndSubstract(arr) {
+    let sumOld = 0;
+    let sumNew = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sumOld += arr[i];
+        if (arr[i] % 2 === 0) {
+            arr[i] += i;
+        } else {
+            arr[i] -= i;
+        }
+        sumNew += arr[i];
+    }
+    console.log(arr);
+    console.log(sumOld);
+    console.log(sumNew);
+}
+addAndSubstract([5, 15, 23, 56, 35]);
