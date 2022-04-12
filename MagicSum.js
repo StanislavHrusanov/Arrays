c5d1cf2a36d051715a5ec034cacb13df326c9b60
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-04-12 19:34:10 +0300

    Add problem solution

---
+++ MagicSum.js
@@ -0,0 +1,13 @@
+function magicSum(arr, number) {
+    for (let i = 0; i < arr.length; i++) {
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if ((arr[i] + arr[j]) === number) {
+                console.log(`${arr[i]} ${arr[j]}`);
+            }
+        }
+    }
+}
+magicSum([1, 2, 3, 4, 5, 6],
+    6
+);
