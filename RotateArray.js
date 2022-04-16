d59813e867d67d64a5d730f6e8ecaac912d91552
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-04-16 11:03:31 +0300

    Add problem solution

---
+++ RotateArray.js
@@ -0,0 +1,10 @@
+function rotateArray(arr) {
+    let rotations = Number(arr.pop());
+
+    for (let i = 0; i < rotations; i++) {
+        let rotatedElement = arr.pop();
+        arr.unshift(rotatedElement);
+    }
+    console.log(arr.join(' '));
+}
+rotateArray(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
